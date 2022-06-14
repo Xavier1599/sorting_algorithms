eeae9ad71e6ee2bbc958b11cde7bc75806e7a33d
Xavier <xavierperez1599@gmail.com>
2022-06-13 23:13:03 -0400

    adding task 0 with new line

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: array to sort
+ * @size: number element of array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i;
+    size_t j;
+    size_t flag;
+    size_t temp;
+
+    if (array == NULL || array == 0)
+        return;
+
+    for (i = 0; i < size; i++)
+    {
+        flag = 0;
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                flag = 1;
+                print_array(array, size);
+            }
+        }
+        if (flag == 0)
+            break;
+    }
+}
